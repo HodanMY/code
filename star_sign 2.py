0be49977c4bbe0465a4a47bec6b53eff1b133109
HodanMY <60483283+HodanMY@users.noreply.github.com>
2020-03-22 19:53:54 +0000

    Update star_sign 2.py

--- star_sign 2.py
+++ star_sign 2.py
@@ -26,69 +26,109 @@ while True:
         libra = "Libra"
         aquarius = "Aquarius"
 
-        fire_element = "\nYour Zodiac element is Fire "
-        water_element = "\nYour Zodiac element is Water"
-        air_element = "\nYour Zodiac element is Air"
-        earth_element = "\nYour Zodiac element is Earth"
-
         
-                
-        if month == 'december':
-                                star_sign = sagittarius if (day < 22) else capricorn 
-                              
-        elif month == 'january':
-                                star_sign = capricorn if (day < 20) else aquarius
-                               
-        elif month == 'february':
-                                star_sign = aquarius if (day < 19) else pisces
-                              
-        elif month == 'march':
-                                star_sign = pisces if (day < 21) else aries
-                              
-        elif month == 'april':
-                                star_sign = aries if (day < 15) else taurus
-                              
-        elif month == 'may':
-                                star_sign = taurus if (day < 21) else  gemini
-                 
-        elif month == 'june':
-                                star_sign = gemini if (day < 21) else cancer                    
-                                
-        elif month == 'july':
-                                star_sign = cancer if (day < 23) else leo
-        
-        elif month == 'august':
-                                star_sign = leo if (day < 23) else virgo
-                                
-        elif month == 'september':
-                                star_sign = virgo if (day < 23) else libra
-                                
-        elif month == 'october':
-                                star_sign = libra if (day < 23) else scorpio
-                              
-        elif month == 'november':
-                                star_sign = scorpio if (day < 22) else sagittarius
-
-        
-
 
         def zodiac_element(x):
-                print("\nYour zodiac element is " + x
+                print("\nYour zodiac element is " + x.title())
+                return
+                
+        if month == 'december':
+                star_sign = sagittarius if (day < 22) else capricorn
+                if star_sign == sagittarius:
+                        zodiac_element('fire')
+                elif star_sign == capricorn:
+                        zodiac_element('earth')
+                              
+        elif month == 'january':
+                star_sign = capricorn if (day < 20) else aquarius
+
+                if star_sign == capricorn:
+                        zodiac_element('earth')
+                elif star_sign == aquarius:
+                        zodiac_element('air')
+                               
+        elif month == 'february':
+                star_sign = aquarius if (day < 19) else pisces
+
+                if star_sign == aquarius:
+                        zodiac_element('air')
+                elif star_sign == pisces:
+                        zodiac_element('water')
+                              
+        elif month == 'march':
+                star_sign = pisces if (day < 21) else aries
+
+                if star_sign == pisces:
+                        zodiac_element('water')
+                elif star_sign == aries:
+                        zodiac_element('fire')
+       
+        elif month == 'april':
+                star_sign = aries if (day < 15) else taurus
+
+                if star_sign == aries:
+                        zodiac_element('fire')
+                elif star_sign == taurus:
+                        zodiac_element('earth')
+                              
+        elif month == 'may':
+                star_sign = taurus if (day < 21) else  gemini
+
+                if star_sign == taurus:
+                        zodiac_element('earth')
+                elif star_sign == gemini:
+                        zodiac_element('air')
+          
+        elif month == 'june':
+                star_sign = gemini if (day < 21) else cancer                    
+
+                if star_sign == gemini:
+                        zodiac_element('air')
+                elif star_sign == cancer:
+                        zodiac_element('water')
+                                
+        elif month == 'july':
+                star_sign = cancer if (day < 23) else leo
+
+                if star_sign == cancer:
+                        zodiac_element('water')
+                elif star_sign == leo:
+                        zodiac_element('fire')
+                  
+        
+        elif month == 'august':
+                star_sign = leo if (day < 23) else virgo
+
+                if star_sign == leo:
+                        zodiac_element('fire')
+                elif star_sign == virgo:
+                        zodiac_element('earth')
+                 
+        elif month == 'september':     
+                star_sign = virgo if (day < 23) else libra
+                
+                if star_sign == virgo:
+                        zodiac_element('earth')
+                elif star_sign == libra:
+                        zodiac_element('air')                   
+        
+        elif month == 'october':
+                star_sign = libra if (day < 23) else scorpio
+                
+                if star_sign == libra:
+                        zodiac_element('air')
+                elif star_sign == scorpio:
+                        zodiac_element('water')                  
+        elif month == 'november':
+                star_sign = scorpio if (day < 22) else sagittarius
+                
+                if star_sign == scorpio:
+                        zodiac_element('water')
+                elif star_sign == sagittarius:
+                        zodiac_element('fire')
+
 
         print("Your Astrological Sign is " + star_sign)
-
-
-        if star_sign == [capricorn, taurus, virgo]:
-                zodiac_element(earth)
-            
-        elif star_sign == [cancer, scorpio, pisces]:
-                zodiac_element(water)
-            
-        elif star_sign == [leo, aries, sagittarius]:
-                 zodiac_element(fire)
-            
-        elif star_sign == [gemini, libra, aquarius]:
-                zodiac_element(air) 
 
         restart = input("Would you like to try again? (y/n)")
 
